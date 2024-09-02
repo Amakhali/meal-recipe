7b8c832022927c15d9194360c56af6a9913cdbaa
unknown <mukodazayofe@gmail.com>
2024-09-02 15:44:15 +0100

    deploying

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import GlobalState from './context/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
       <GlobalState>
         <App />
       </GlobalState>
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 )
